ee0356272fae2cb4c0e5b3c781341b8f96fc012c
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-09-15 12:45:28 +0530

    Almost there

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,6 +4,8 @@ const handOptions = {
     "scissors": "assets/Scissors.png"
   }
 
+let SCORE = 0
+
 const pickUserHand = (hand) => {
     let hands = document.querySelector(".hands");
     hands.style.display = "none";
@@ -18,6 +20,7 @@ const pickUserHand = (hand) => {
     document.getElementById("userPickImage").src = handOptions[hand];
 
     let cpHand = pickComputerHand();
+    referee(hand, cpHand)
 
 }
 
@@ -31,17 +34,52 @@ const pickComputerHand = () => {
 }
 
 const referee = (userHand, cpHand) => {
-    if (userHand == "paper" && cpHand=="scissors"){
-        setDecision("YOU LOSE!")
-    } else if (userHand == "paper" && cpHand=="rock"){
-        setDecision("YOU WIN!")
+    if (userHand == "paper" && cpHand == "scissors") {
+        setDecision("YOU LOSE!");
+    }
+    if (userHand == "paper" && cpHand == "rock") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+    }
+    if (userHand == "paper" && cpHand == "paper") {
+        setDecision("It's a tie!");
+    }
+    if (userHand == "rock" && cpHand == "scissors") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+    }
+    if (userHand == "rock" && cpHand == "paper") {
+        setDecision("YOU LOSE!");
+    }
+    if (userHand == "rock" && cpHand == "rock") {
+        setDecision("It's a tie!");
+    }
+    if (userHand == "scissors" && cpHand == "scissors") {
+        setDecision("It's a tie!");
+    }
+    if (userHand == "scissors" && cpHand == "rock") {
+        setDecision("YOU LOSE!");
+    }
+    if (userHand == "scissors" && cpHand == "paper") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
     }
 }
 
+const restartGame = () => {
+    let hands = document.querySelector(".hands");
+    hands.style.display = "flex";
+
+    let contest = document.querySelector(".contest");
+    contest.style.display = "none";
+
+}
+
 const setDecision = (decision) => {
-    console.log(decision);
+    document.querySelector(".decision h1").innerText = decision
 }
 
 const setScore = (score) => {
-    console.log(score)
+    SCORE = score;
+    document.querySelector(".score h1").innerText = score
 }
